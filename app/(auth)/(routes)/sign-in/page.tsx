5baee8754783c56ee7a823f2526d4c1008d20084
kashishnaaz <kashishn457@gmail.com>
2026-04-04 19:45:12 +0530

    feat: initial commit

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const signInPage = () => {
+    return (
+        <div>
+            <h1>Sign In</h1>
+        </div>
+    )
+}
+
+export default signInPage;
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <p className="text-2xl font-bold text-center mt-10 *:text-gray-700">
+      this is protected page
+    </p>
+  );
+}
+
+
